8ab5b412772df1939a5c8cf8975c6d77971949ae
CristianoV <cristianoviieira@gmail.com>
2022-10-23 11:36:06 -0300

    feat: adiciona classe abstrata da camada de model

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,30 @@
+import { Model, UpdateQuery } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+export default abstract class MongoModel<T> implements IModel<T> {
+  constructor(protected model: Model<T>) {}
+
+  public async create(data: Required<T>): Promise<T> {
+    return this.model.create({ ...data });
+  }
+
+  public async read(): Promise<T[]> {
+    return this.model.find();
+  }
+
+  public async readOne(id: string): Promise<T | null> {
+    return this.model.findOne({ id });
+  }
+
+  public async update(id: string, data: T): Promise<T | null> {
+    return this.model.findByIdAndUpdate(
+      id,
+      { ...data } as UpdateQuery<T>,
+      { new: true },
+    );
+  }
+
+  public async delete(id: string): Promise<T | null> {
+    return this.model.findOneAndDelete({ id });
+  }
+}
