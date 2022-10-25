cc8438e4a38dc8d5f36b5c96a066c680ff1c9013
CristianoV <cristianoviieira@gmail.com>
2022-10-25 16:29:59 -0300

    feat: cria novas rotas para as pesquisas pelo model de carros

---
+++ src/routes/CarRoute.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import CarController from '../controllers/CarController';
+import CarModel from '../models/CarModel';
+import CarService from '../services/CarService';
+
+const route = Router();
+
+const car = new CarModel();
+const carService = new CarService(car);
+const carController = new CarController(carService);
+
+route.post('/cars', (req, res) => carController.create(req, res));
+route.get('/cars', (req, res) => carController.read(req, res));
+route.get('/cars/:id', (req, res) => carController.readOne(req, res));
+
+export default route;
