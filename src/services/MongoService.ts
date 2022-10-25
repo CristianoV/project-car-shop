e3ebfa2c8da200b3f8b5c59dbf1a7a0b5d30d855
CristianoV <cristianoviieira@gmail.com>
2022-10-25 16:18:12 -0300

    feat: adiciona função na camada de service que adiciona um novo carro no sistema

---
+++ src/services/MongoService.ts
@@ -0,0 +1,21 @@
+import { Schema } from 'zod';
+import { ErrorTypes } from '../errors/catalog';
+import { IModel } from '../interfaces/IModel';
+import IService from '../interfaces/IService';
+
+export default abstract class CarService<T> implements IService<T> {
+  constructor(
+    private model: IModel<T>,
+    private schema: Schema<T>,
+  ) {}
+
+  public async create(obj: unknown): Promise<T> {
+    const parsed = this.schema.safeParse(obj);
+    
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+
+    return this.model.create(parsed.data);
+  }
+}
