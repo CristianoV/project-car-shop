c74b01766609c6608847cb487210b7167806c4a4
CristianoV <cristianoviieira@gmail.com>
2022-10-25 16:43:42 -0300

    feat: adiciona middleware que busca e trata erros

---
+++ src/middlewares/error.ts
@@ -0,0 +1,23 @@
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import { ErrorTypes, errorCatalog } from '../errors/catalog';
+
+const errorHandler: ErrorRequestHandler = (err: Error | ZodError, _req, res, _next) => {
+  if (err instanceof ZodError) { 
+    const { message } = err.issues[0];
+    return res.status(400).json({ message });
+  }
+
+  const messageAsErrorType = err.message as keyof typeof ErrorTypes;
+  const mappedError = errorCatalog[messageAsErrorType];
+  
+  if (mappedError) {
+    const { httpStatus, error } = mappedError;
+    
+    return res.status(httpStatus).json({ error });
+  }
+  console.error(err);
+  return res.status(500).json({ message: 'internal error' });
+};
+
+export default errorHandler;
