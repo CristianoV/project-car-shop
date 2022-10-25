ac9ff2f1e6e9a6618e967be9ab6434791c9c2be2
CristianoV <cristianoviieira@gmail.com>
2022-10-25 20:24:37 -0300

    feat: adiciona pasta de mocks

---
+++ src/tests/unit/utils/mocks.ts
@@ -0,0 +1,58 @@
+export const allCarsMock = [
+	{
+		"_id": "635860a7835b786358594537",
+		"doorsQty": 2,
+		"seatsQty": 2,
+		"model": "carro 1",
+		"year": 1963,
+		"color": "red",
+		"buyValue": 3500
+	},
+	{
+		"_id": "635860ae835b78635859453a",
+		"doorsQty": 2,
+		"seatsQty": 2,
+		"model": "carro 2",
+		"year": 1963,
+		"color": "red",
+		"buyValue": 3500
+	},
+	{
+		"_id": "635860b0835b78635859453c",
+		"doorsQty": 2,
+		"seatsQty": 2,
+		"model": "carro 3",
+		"year": 1963,
+		"color": "red",
+		"buyValue": 3500
+	}
+];
+
+export const newCarMock = {
+    "model": "carro com update",
+    "year": 1963,
+    "color": "red",
+    "buyValue": 3500,
+    "seatsQty": 2,
+    "doorsQty": 2
+  }
+
+  export const newCarMockWithId = {
+	"doorsQty": 2,
+	"seatsQty": 2,
+	"model": "carro 1",
+	"year": 1963,
+	"color": "red",
+	"buyValue": 3500,
+	"_id": "63586114835b78635859453f"
+}
+
+export const newCarMockUpdated = {
+	"doorsQty": 2,
+	"seatsQty": 2,
+	"model": "carro com update",
+	"year": 1963,
+	"color": "red",
+	"buyValue": 3500,
+	"_id": "63586114835b78635859453f"
+}
