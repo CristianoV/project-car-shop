ed14eb2bd67bb9993c697a44373343731a3db16d
CristianoV <cristianoviieira@gmail.com>
2022-10-25 18:35:40 -0300

    feat: adiciona nova função na camada de controller

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -24,4 +24,9 @@ export default class CarController {
     const results = await this.service.update(req.params.id, req.body);
     return res.status(200).json(results);
   }
+
+  public async delete(req: Request, res: Response) {
+    await this.service.delete(req.params.id);
+    return res.status(204).json();
+  }
 }
